08598072b3989ce3bd74d293c69c223c02c9272d
tzw-madzia <madzia.nowak.pl@gmail.com>
2014-08-24 15:00:04 +0200

    [#77505630] display categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   root 'welcome#index'
+  match '/categories', to: 'categories#index', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
